b9a0296f9bceedb7833f8568d9f9c4db05b58337
Rafael Carrasco <rscarrasco@proton.me>
2022-09-26 20:32:03 -0300

    Garante criação da tabela de produtos

--- src/database.php
+++ src/database.php
@@ -14,6 +14,28 @@ function connect($config){
     }
 
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    create_table_if_not_exists($conn);
     return $conn;
+}
+
+function create_table_if_not_exists($connection){
+    $sql = <<<SQL
+CREATE TABLE IF NOT EXISTS products(
+    code         VARCHAR(14)                NOT NULL,
+    barcode      VARCHAR(30),
+    status       ENUM('draft', 'imported'),
+    imported_t   DATETIME,
+    url          VARCHAR(120),
+    product_name VARCHAR(200),
+    quantity     VARCHAR(20),
+    categories   VARCHAR(200),
+    packaging    VARCHAR(50),
+    brands       VARCHAR(30),
+    image_url    VARCHAR(200),
+    UNIQUE(code)
+);
+SQL;
+
+    $connection->exec($sql);
 }
 
